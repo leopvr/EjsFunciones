8c290b986733853bce1cf6ea8697a8c431ae8221
leopvr <leo.pvr13@gmail.com>
2022-11-01 10:34:15 -0300

    :sparkles: Feat: exercises with their solutions

---
+++ script.js
@@ -0,0 +1,130 @@
+// 1) Crear una función que tome dos parámetros y devuelva un console.log() con el resultado de la suma dentro del bloque de ejecución de la función. Invocar la función al menos tres veces
+// function ej1(param1, param2) {
+//     return param1 + param2;
+// }
+// console.log(ej1(1, 3))
+// console.log(ej1(4, 6))
+// console.log(ej1(5, 56))
+
+// 2) Crear una función que tome dos parámetros y devuelva un console.log() con el resultado de la resta dentro del bloque de ejecución de la función. Invocar la función al menos tres veces
+// function ej2(param1, param2) {
+//     return param1 - param2;
+// }
+// console.log(ej2(1, 3))
+// console.log(ej2(4, 6))
+// console.log(ej2(5, 56))
+
+// 3) Crear una función que tome dos parámetros y devuelva un console.log() con el resultado de la multiplicación dentro del bloque de ejecución de la función. Invocar la función al menos tres veces
+// function ej3(param1, param2) {
+//     return param1 * param2;
+// }
+// console.log(ej3(1, 3))
+// console.log(ej3(4, 6))
+// console.log(ej3(5, 56))
+
+// 4) Crear una función que tome dos parámetros y devuelva un console.log() con el resultado de la división dentro del bloque de ejecución de la función. Invocar la función al menos tres veces
+// function ej4(param1, param2) {
+//     return param1 / param2;
+// }
+// console.log(ej4(1, 3))
+// console.log(ej4(4, 6))
+// console.log(ej4(5, 56))
+
+// 5) Crear un array con al menos cinco frutas y crear una función que recorra el array al ser invocada, devolviendo los nombres de las frutas por consola, dentro del bloque de ejecución.
+// const arrayFrutas = ["manzana", "pera", "anana", "banana", "sandia"];
+// function ej5() {
+//     for (let i = 0; i < arrayFrutas.length; i++) {
+//         console.log(arrayFrutas[i]);
+//     }
+// }
+// ej5();
+
+// 6) Crear una función que recorra un array de diez animales y devuelva los nombres por consola dentro del bloque de ejecución
+// const arrayAnimales = ["perro", "gato", "zorro", "vaca", "toro", "caballo", "jirafa", "delfin", "elefante", "leon"];
+// function ej6() {
+//     for (let i = 0; i < arrayAnimales.length; i++) {
+//         console.log(arrayAnimales[i]);
+//     }
+// }
+// ej6();
+
+// 7) Crear una función que permite reducir los puntos 1 al 4 en una sola función, por lo cual deberá tomar tres parámetros: Los valores a procesar y la operación aritmética a resolver.
+// function ej7(param1, param2) {
+//     let suma = param1 + param2;
+//     let resta = param1 - param2;
+//     let multi = param1 * param2;
+//     let divi = param1 / param2;
+//     return {suma, resta, multi, divi};
+// }
+// console.log(ej7(10, 20))
+
+// 8) Crear una función que tome el primer elemento del array obtenido en https: /jsonplaceholder.typicode.com/users y que retorne el nombre del usuario con su correo. La función debe mostrar el resultado por consola al momento de ser invocada.
+// const urlAPI = fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((users) => {
+//     function ej8() {
+//         console.log(`El nombre del primer usuario es "${users[0].name}" y el mail es "${users[0].email}"`);
+//     }
+//     ej8();
+//   });
+
+// 9) Pedirle cinco números al usuario, almacenarlos para luego crear una función que los ordene de menor a mayor y otra función que los ordene de mayor a menor
+// var pNum = prompt("Coloca 5 numeros porfavor, aca el primero");
+// var sNum = prompt("Ahora el segundo");
+// var tNum = prompt("Ahora el tercero");
+// var cNum = prompt("Ahora el cuarto");
+// var qNum = prompt("Y por ultimo el quinto");
+
+// const numeros = [...pNum, ...sNum, ...tNum, ...cNum, ...qNum];
+
+// function ej9() {
+//     console.log(numeros.sort())
+// }
+// ej9();
+
+// function ej9_1() {
+//     console.log(numeros.sort().reverse())
+// }
+// ej9_1();
+
+
+// 10)¿Qué son los eventos en JS? Buscar información y debatir en el grupo sobre el funcionamiento de:
+// onclick
+// onchange
+// onkeydown
+// onmouseover
+// onsubmit
+// onscroll
+
+// 11) Crear una función que cambie una cadena de texto a minúsculas (sin importar el formato de ingreso)
+// function ej11() {
+//     let cadText = prompt("Ingrese cualquier frase o palabra");
+//     let lowerText = cadText.toLowerCase();
+//     console.log(lowerText)
+// }
+// ej11();
+
+// 12) Crear una función a gusto y que resuelva un problema que gusten.
+function ej12() {
+  const urlAPI = fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => {
+      return response.json();
+    })
+
+    .then((users) => {
+
+      let arrayUser = [];
+      
+      function ej12_1() {
+        users.forEach((e) => {
+          arrayUser.push(e);
+        });
+      }
+      ej12_1();
+
+      console.table(arrayUser);
+
+    });
+}
